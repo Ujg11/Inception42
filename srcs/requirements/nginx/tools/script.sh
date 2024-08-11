7fef441d01412ba7aa2825679dbbe2516c2976b2
Ujg11 <126014096+Ujg11@users.noreply.github.com>
2024-08-11 14:15:12 +0200

    Update script.sh

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f /etc/ssl/jareste.42.fr.crt ]; then
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/ojimenez.42.fr.crt -keyout /etc/ssl/ojimenez.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=Barcelona/CN=ojimenez.42.fr";
+fi
+exec "$@"
+
 # Iniciem nginx en primer pla (mode daemon es en segon pla)
-nginx -g "daemon off;"
+#nginx -g "daemon off;"
