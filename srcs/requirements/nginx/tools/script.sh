377f0599419331bab98c19c93418598cda897525
root <root@ojimenez42.ojimenez.42.fr>
2024-08-11 15:10:21 +0200

    Nginx arreglat

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f /etc/ssl/jareste.42.fr.crt ]; then
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/ojimenez.42.fr.crt -keyout /etc/ssl/ojimenez.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=Barcelona/CN=ojimenez.42.fr";
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/ojimenez.42.fr.pem -keyout /etc/ssl/ojimenez.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=Barcelona/CN=ojimenez.42.fr";
 fi
 exec "$@"
 
