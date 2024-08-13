6efbc4d1a5d16f4793146b386527ea4afb538f1a
ojimenez <orioljg2002@gmail.com>
2024-08-13 11:38:38 +0200

    Mariadb arreglada creo

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-#Inicialitzem la base de dades
-mysqld --initialize-insecure
-
-#Iniciem Mariadb
-/etc/init.d/mysql start
+service mariadb start
 
 #Mirem si ja existeix la base de dades
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
@@ -20,32 +16,30 @@ else
 	#Eliminar base de dades prova
 	#Recarregar les taules de privilegis?
 	mysql_secure_installation << FINISH
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
 n
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+Y
 Y
 Y
 FINISH
 
 	#Donem accés al usuari root desde qualsevol host
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+	mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
 
 	#Creem la BDD i l'usuari de Wordpress. Aqui ens conectarem amb el .sql per crear la bdd.
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
-	GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' \
-	IDENTIFIED BY '$MYSQL_PASSWORD'; \
-	FLUSH PRIVILEGES;" | mysql -uroot
-
-	#Importem la base de dades de WordPress
-	mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+	mysql -e "FLUSH PRIVILEGES;"
+	echo "User created in db successfully"
 
 fi
 
 #Aturem Mariadb que estava en segon pla
-/etc/init.d/mysql stop
+service mariadb stop
 
-#Executem i iniciem MariaDB en primer pla
-exec "$@"
+#Executem
+mysqld
 
