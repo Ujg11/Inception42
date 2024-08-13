f804154deb863ef0d458d96206a8ed89cd397ed5
ojimenez <orioljg2002@gmail.com>
2024-08-13 13:39:22 +0200

    Arreglado

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /etc/ssl/jareste.42.fr.crt ]; then
+if [ ! -f /etc/ssl/ojimenez.42.fr.crt ]; then
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/ssl/ojimenez.42.fr.pem -keyout /etc/ssl/ojimenez.42.fr.key -subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=Barcelona/CN=ojimenez.42.fr";
 fi
 exec "$@"
